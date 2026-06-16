1c33ca611dadd145c3e3acbeeff540d5273c8a58
alexsander <alexsander@alexsander-Inspiron-15-3530>
2026-06-16 13:21:18 -0300

    Inicio

--- Byte & Brew/src/br/edu/cafeteria/app/Principal.java
+++ Byte & Brew/src/br/edu/cafeteria/app/Principal.java
@@ -1,9 +1,8 @@
-package teste;
+package br.edu.cafeteria.app;
 
-public class Teste1 {
+public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("ola");
 		// TODO Auto-generated method stub
 
 	}
