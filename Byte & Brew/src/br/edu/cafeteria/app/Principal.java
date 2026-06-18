bb7866ada1ae0437cc573ed8dec2606145db5b23
alexsander <alexsander@alexsander-Inspiron-15-3530>
2026-06-18 17:25:23 -0300

    Main

--- Byte & Brew/src/br/edu/cafeteria/app/Principal.java
+++ Byte & Brew/src/br/edu/cafeteria/app/Principal.java
@@ -1,13 +1,19 @@
 package br.edu.cafeteria.app;
 
+
 import br.edu.cafeteria.modelo.cadastros.*;
-import br.edu.cafeteria.modelo.produto.Bebidas;
-import br.edu.cafeteria.modelo.produto.Comidas;
+import br.edu.cafeteria.modelo.atendente.*;
+import br.edu.cafeteria.modelo.produto.*;
+import java.util.Scanner;
 
 public class Principal {
 
     public static void main(String[] args) {
+    	Scanner scanner = new Scanner(System.in);
         CadastroProduto cardapio = new CadastroProduto();
+        Atendente atendente = new Atendente("Lucas");
+        Atendente atendente2 = new Atendente("Pedro");
+        Atendente atendente3 = new Atendente("Tiago");
         
         cardapio.adicionarProduto(new Bebidas("Café do Programador", 8.50, 101, 15, "M", 120, true));
         cardapio.adicionarProduto(new Bebidas("Poção de Mana", 7.00, 102, 10, "G", 0, false));
@@ -15,8 +21,17 @@ public class Principal {
         cardapio.adicionarProduto(new Comidas("Portal Cake", 15.00, 202, 5, 30, false, true));
         
         
-        System.out.println("--- BEM VINDO A CAFETERIA BYTE & BREW ---");
+        System.out.println("--- BEM VINDO A CAFETERIA BYTE & BREW ! ---");
+        System.out.println("Escolha seu Atendente");
+        String escolha = scanner.next();
+        
+        System.out.println("Olá meu nome é "+ escolha +" e serei seu atendente");
+        
         System.out.println("Esse é o nosso cardápio:");
+        System.out.println("---CARDÁPIO---");
         cardapio.listarProdutos();
+        
+        System.out.println("Digite o codigo do produto");
+        int codigo = scanner.nextInt();
     }
 }
