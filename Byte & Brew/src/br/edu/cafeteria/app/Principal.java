e8f35dc7fa88575c8eeefc0e4b706887044ccefc
alexsander <alexsander@alexsander-Inspiron-15-3530>
2026-06-18 10:38:06 -0300

    MainIniciada

--- Byte & Brew/src/br/edu/cafeteria/app/Principal.java
+++ Byte & Brew/src/br/edu/cafeteria/app/Principal.java
@@ -1,10 +1,22 @@
 package br.edu.cafeteria.app;
 
+import br.edu.cafeteria.modelo.cadastros.*;
+import br.edu.cafeteria.modelo.produto.Bebidas;
+import br.edu.cafeteria.modelo.produto.Comidas;
+
 public class Principal {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        CadastroProduto cardapio = new CadastroProduto();
+        
+        cardapio.adicionarProduto(new Bebidas("Café do Programador", 8.50, 101, 15, "M", 120, true));
+        cardapio.adicionarProduto(new Bebidas("Poção de Mana", 7.00, 102, 10, "G", 0, false));
+        cardapio.adicionarProduto(new Comidas("Lembas Bread", 12.00, 201, 8, 20, true, false));
+        cardapio.adicionarProduto(new Comidas("Portal Cake", 15.00, 202, 5, 30, false, true));
+        
+        
+        System.out.println("--- BEM VINDO A CAFETERIA BYTE & BREW ---");
+        System.out.println("Esse é o nosso cardápio:");
+        cardapio.listarProdutos();
+    }
 }
